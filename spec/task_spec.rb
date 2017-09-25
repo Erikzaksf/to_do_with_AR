6416ceb0615404af01cbfad3ef47d7f7fd3f515d
Erik Zakrzewski and Luciano Oliveira <erik+luciano@>
2017-09-25 13:17:13 -0700

    todo app with AR completed

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -17,6 +17,14 @@ require('spec_helper')
     end
   end
 
+  describe("#list") do
+    it("tells which list it belongs to") do
+      test_list = List.create({:name => "list"})
+      test_task = Task.create({:description => "task", :list_id => test_list.id})
+      expect(test_task.list()).to(eq(test_list))
+    end
+  end
+
   # describe(".all") do
   #   it("is empty at first") do
   #     expect(Task.all).to(eq([]))
